59274c631162872665d209744fba30325f69c0b9
HwiNyeon Kim <z2b23173us@gmail.com>
2025-06-18 23:16:07 +0900

    fix: fix wrong import

--- be/app/db/base.py
+++ be/app/db/base.py
@@ -1,4 +1,4 @@
 """Import all models here for Alembic to detect them."""
-from app.db.base_class import Base  # noqa
+from app.models.base import Base  # noqa
 from app.models.project import Project  # noqa
 from app.models.task import Task  # noqa
