4a798f80a07ade179cfb7cbca30cd022cdc1e09a
HwiNyeon Kim <z2b23173us@gmail.com>
2025-06-19 01:22:17 +0900

    feature: add auth-related utils and deps codes

--- be/app/api/dependencies.py
+++ be/app/api/dependencies.py
@@ -1,4 +1,19 @@
+from typing import Optional
+
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from jose.exceptions import JWTError
+from jose.jwt import decode
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.config.settings import settings
 from app.db import AsyncSessionLocal
+from app.models import User as UserModel
+from app.schemas import User
+
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl=f"{settings.API_V1_STR}/auth/login"
+)
 
 
 async def get_db():
@@ -8,3 +23,47 @@ async def get_db():
             yield session
         finally:
             await session.close()
+
+
+async def get_current_user(
+    db: AsyncSession = Depends(get_db), token: str = Depends(oauth2_scheme)
+):
+    credentials_exception = HTTPException(
+        status_code=status.HTTP_401_UNAUTHORIZED,
+        detail="Could not validate credentials",
+        headers={"WWW-Authenticate": "Bearer"},
+    )
+    try:
+        payload = decode(
+            token, settings.SECRET_KEY, algorithms=[settings.ALGORITHM]
+        )
+        user_id: Optional[str] = payload.get("sub")
+
+        if user_id is None:
+            raise credentials_exception
+    except JWTError:
+        raise credentials_exception
+
+    user = await db.query(UserModel).filter(UserModel.id == user_id).first()
+    if user is None:
+        raise credentials_exception
+
+    if not user.is_active:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Inactive user",
+        )
+
+    return user
+
+
+async def get_current_active_superuser(
+    current_user: User = Depends(get_current_user),
+):
+    if not current_user.is_superuser:
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="The user doesn't have enough privileges",
+        )
+
+    return current_user
